2cfb19268539df370cf35fa854f58da215ae1aed
Neal <149696795+Neal-Bhattacharya@users.noreply.github.com>
2025-08-31 03:05:25 -0400

    Add files via upload

--- grapher/grapher.py
+++ grapher/grapher.py
@@ -1,3 +1,5 @@
+import random
+
 import pygraphviz as pgv
 import networkx as nx
 
@@ -7,19 +9,42 @@ G = pgv.AGraph(directed=True)
 
 p = Processor()
 p.run()
-print(p.get_edges())
+
+edges = p.get_edges()
+colors = set()
+author_colors = {}
 print("Generating graph...")
-for e in p.get_edges():
+def get_color():
+    r = lambda: random.randint(0,255)
+    color = '#%02X%02X%02X' % (r(),r(),r())
+    while color in colors:
+        color = '#%02X%02X%02X' % (r(), r(), r())
+    colors.add(color)
+    return color
+
+for e in edges:
+    if e[0] not in author_colors:
+        author_colors[e[0]] = get_color()
+    if e[1] not in author_colors:
+        author_colors[e[1]] = get_color()
+
+for e in edges:
     G.add_edge(e[0], e[1])
     edge = G.get_edge(e[0], e[1])
     edge.attr['label'] = e[2]
     edge.attr['fontname'] = 'Didot'
-    edge.attr['tooltip'] = "{} mentioned by {} {} time{}".format(e[0], e[1], e[2], "s" if e[2] > 1 else "")
-for node in G.nodes(): node.attr['fontname'] = 'Didot'
+    edge.attr['color'] = author_colors[e[0]]
+    tooltip = "{} mentioned by {} {} time{}".format(e[0], e[1], e[2], "s" if e[2] > 1 else "")
+    edge.attr['tooltip'] = tooltip
+
+for node in G.nodes():
+    node.attr['fontname'] = 'Didot'
+    node.attr['color'] = author_colors[node]
+
 
 G.layout(prog='dot')
 G.draw('graph.png')
-G.draw('graph.svg')
+G.draw('graph.svg', args='')
 N = nx.DiGraph(G)
 max_out = ("", -1)
 max_in = ("", -1)
